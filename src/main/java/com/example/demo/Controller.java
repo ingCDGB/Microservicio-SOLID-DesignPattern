f280c4944240e0df7055f9a2e2614650770300a1
Carlos David <ing.carlosgarciabendahan@gmail.com>
2020-08-30 12:17:01 -0400

    Agregado LOMBOK y utilizando @Slf4j

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -3,17 +3,25 @@ package com.example.demo;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping( value = "")
+@Slf4j
 public class Controller{
+
+    private static final org.slf4j.Logger log =
+            org.slf4j.LoggerFactory.getLogger(Controller.class);
 
     @GetMapping("/hola")
     public String hola(){
+
+        log.info(" ----- HOLA ----- ");
         return "hola";
     }
     @GetMapping("/chao")
     public String chao(){
+        log.info(" ----- CHAO ----- ");
         return "chao";
     }
 }
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @SpringBootApplication
 public class DemoApplication {
 
+	private static final org.slf4j.Logger log =
+			org.slf4j.LoggerFactory.getLogger(DemoApplication.class);
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoApplication.class, args);
+
+		log.info(" ----- APPLICATION IS RUNNING ----- ");
 	}
 
 }
