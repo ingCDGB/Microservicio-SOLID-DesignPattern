bd999217645564cda6d2cd6550258ad786dac7bf
Carlos David <ing.carlosgarciabendahan@gmail.com>
2020-09-06 14:44:58 -0300

    Actualizando dependencia de lombok y utilizando correctamente

--- src/main/java/com/example/demo/DemoService.java
+++ src/main/java/com/example/demo/DemoService.java
@@ -1,9 +1,6 @@
 package com.example.demo;
 
-import java.lang.*;
-
 public interface DemoService{
-
 
     public String helloWorld(String mensaje);
     public String goodByeWorld(String mensaje);
--- src/main/java/com/example/demo/DemoServiceImpl.java
+++ src/main/java/com/example/demo/DemoServiceImpl.java
@@ -1,16 +1,11 @@
 package com.example.demo;
 
-import java.lang.*;
-
-
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class  DemoServiceImpl implements DemoService{
-
-
-    private static final org.slf4j.Logger log =
-            org.slf4j.LoggerFactory.getLogger(DemoServiceImpl.class);
 
     @Override
     public String helloWorld(String mensaje){
